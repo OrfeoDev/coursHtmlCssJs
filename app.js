467d697a02a86679bdee8d6eb7dadba05a27be95
OrfeoDev <orfeo34@icloud.com>
2022-12-09 18:00:26 +0100

    installation navbar responsive

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
+const btnMenu = document.querySelector('.logo-menu');
 const menu = document.querySelector('.liste-nav');
-const btnMenu = document.querySelector('.btn-toggle_container');
 
-btnMenu.addEventListener('click',()=>{
-    menu.classList.toggle('active-menu')
+
+btnMenu.addEventListener('click', () => {
+    menu.classList.toggle('active')
+})
+
+const allLinks = document.querySelectorAll('.item-nav');
+allLinks.forEach(item => {
+    item.addEventListener('click', () => {
+        menu.classList.toggle('active')
+    })
 })
